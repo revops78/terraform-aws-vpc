61f8582fe8674acae5afe517a1f3a19dab238372
revanth <revanth.sn@gmail.com>
2025-08-21 13:17:20 +0100

    updates

--- locals.tf
+++ locals.tf
@@ -4,6 +4,10 @@ locals {
         project = var.project
         Terraform = "true"
     }
-az_names=slice(data.aws_availability_zones.available.names,0,2)# i got error here today because i keep "" for the slice,it is a list for the list we cant keep like that 
+
+}
+
+locals {
+  az_names=slice(data.aws_availability_zones.available.names,0,2)# i got error here today because i keep "" for the slice,it is a list for the list we cant keep like that 
     
 }
